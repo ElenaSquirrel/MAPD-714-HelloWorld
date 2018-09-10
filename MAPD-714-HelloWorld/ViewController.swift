5339e6134888edfe99f2dca02909b78dc6c47e95
Elena Melnikova <elenamelnikova@owners-macbook-pro.local>
2018-09-10 14:05:42 -0400

    Initial Commit

---
+++ MAPD-714-HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD-714-HelloWorld
+//
+//  Created by Elena Melnikova on 2018-09-10.
+//  Copyright © 2018 Elena Melnikova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
